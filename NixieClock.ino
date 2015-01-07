d68b28c9bafa1a641df92ec0a8aa1a4eb0bf2124
Torsten Amshove <torsten@amshove.net>
2015-01-07 21:10:21 +0100

    Kleine fixes und Funktion fuer Buttons

--- NixieClock.ino
+++ NixieClock.ino
@@ -1,4 +1,4 @@
-/*
+ /*
  *
  *
  */
@@ -6,7 +6,7 @@
 #include "DS1307RTC.h"
 #include "Wire.h"
 #include "Time.h"
-#include "dcf77.h"
+//#include "dcf77.h"
 
 //////////////
 // Settings //
@@ -50,39 +50,79 @@ void setup() {
   }
   for(uint8_t i=0; i<6; i++){
     pinMode(pins_tubes[i], OUTPUT);
+    digitalWrite(pins_tubes[i],LOW);
   }
   for(uint8_t i=0; i<2; i++){
     pinMode(pins_dots[i], OUTPUT);
   }
   
+  pinMode(PIN_DCF77, INPUT);
+  
+  setTime(RTC.get());
+  
+/*
   // DCF77
   pinMode(PIN_DCF77, INPUT);
   digitalWrite(PIN_DCF77, HIGH);
   DCF77_Clock::setup();
   DCF77_Clock::set_input_provider(dcf77_input_provider);
+  
+  // Wait till clock is synced, depending on the signal quality this may take
+  // rather long. About 5 minutes with a good signal, 30 minutes or longer
+  // with a bad signal
+  DCF77_Clock::time_t DCFtime;
+  for (uint8_t state = DCF77::useless;
+    state == DCF77::useless || state == DCF77::dirty;
+    state = DCF77_Clock::get_clock_state()) {
+
+    // wait for next sec
+    DCF77_Clock::get_current_time(DCFtime);
+  }
+  
+  setTime(BCD::bcd_to_int(DCFtime.hour),BCD::bcd_to_int(DCFtime.minute),BCD::bcd_to_int(DCFtime.second),BCD::bcd_to_int(DCFtime.day),BCD::bcd_to_int(DCFtime.month),BCD::bcd_to_int(DCFtime.year));
+  RTC.set(now());
+*/
 }
 
 //////////
 // Loop //
 //////////
+bool button_pressed = false;
 void loop() {
   // DCF77
-  DCF77_Clock::time_t DCFtime;
+/*  DCF77_Clock::time_t DCFtime;
   DCF77_Clock::get_current_time(DCFtime);
   if(DCFtime.month.val > 0){
     setTime(BCD::bcd_to_int(DCFtime.hour),BCD::bcd_to_int(DCFtime.minute),BCD::bcd_to_int(DCFtime.second),BCD::bcd_to_int(DCFtime.day),BCD::bcd_to_int(DCFtime.month),BCD::bcd_to_int(DCFtime.year));
     RTC.set(now());
   }
+ */
+
+  // Set time with switches
+  int switches = analogRead(PIN_DCF77);
+  if(switches < 700){
+    if(switches > 547 && !button_pressed){ // Hour
+      adjustTime(3600);
+      RTC.set(now());
+    }else if(switches > 410 && !button_pressed){ // Minute
+      adjustTime(60);
+      RTC.set(now());    
+    }else if(!button_pressed){  // Second
+      adjustTime(1);
+      RTC.set(now());
+    }
+    button_pressed = true;
+  }else button_pressed = false;
   
   // Get time from RTC
   time_t rtc_time = RTC.get();
   uint8_t display_value[6] = {0,0,0,0,0,0};
-  display_value[0] = hour(rtc_time)/10;
-  display_value[1] = hour(rtc_time)%10;
-  display_value[2] = minute(rtc_time)/10;
-  display_value[3] = minute(rtc_time)%10;
-  display_value[4] = second(rtc_time)/10;
-  display_value[5] = second(rtc_time)%10;
+  display_value[5] = hour(rtc_time)/10;
+  display_value[4] = hour(rtc_time)%10;
+  display_value[3] = minute(rtc_time)/10;
+  display_value[2] = minute(rtc_time)%10;
+  display_value[1] = second(rtc_time)/10;
+  display_value[0] = second(rtc_time)%10;
  
   // Show Value on Tubes
   multiplex(display_value); 
@@ -95,12 +135,22 @@ void loop() {
 /////////////////////////////  
 void multiplex(uint8_t *display_value) {
   for(uint8_t t=0; t<6; t++){
-    for(uint8_t i=0; i<6; i++){
-      digitalWrite(pins_tubes[i],t==i);
-    }
-
     for(uint8_t i=0; i<4; i++){
       digitalWrite(pins_character[i],char_to_bin[display_value[t]][i]);
     }
+    digitalWrite(pins_tubes[t],HIGH);
+    delay(2);
+    digitalWrite(pins_tubes[t],LOW);
+
+/*
+    // Dots
+    if(display_value[0] % 2){
+      digitalWrite(pins_dots[0],HIGH);
+      digitalWrite(pins_dots[1],HIGH);
+    }else{
+      digitalWrite(pins_dots[0],LOW);
+      digitalWrite(pins_dots[1],LOW);
+    }
+*/
   }
 }
